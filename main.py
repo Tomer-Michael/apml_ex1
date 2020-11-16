47ca2acad5d743a5d564601de960b344234c7bca
Tomer Michael <tmichael@lightricks.com>
2020-11-16 20:14:29 +0200

    Small update.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 import torch
+import torch.nn.functional as F
 import torchvision as torchvision
 from torch import nn, optim
 from torch.utils.data import DataLoader
@@ -164,33 +165,56 @@ def _test_internal(checkpoint_name, path_to_dataset):
     print('Accuracy of the network on the ' + str(len(data_loader)) + ' test images: %d %%' % (100 * correct / total))
 
 
-def findAdversarialExample(algorithm_name='FGSM',
-                           checkpoint_name='pre_trained', path_to_dataset='dev.pickle', epsilons=None):
+def findAdversarialExample(attacker_name='FGSM',
+                           use_cuda=False, checkpoint_name='pre_trained', path_to_dataset='dev.pickle', epsilons=None):
     if epsilons is None:
         epsilons = [0, .05, .1, .15, .2, .25, .3]
 
     model = create_model(checkpoint_name=checkpoint_name, is_in_eval_mode=True)
-
     data_loader = create_data_loader(path_to_dataset, False, 1, True, 2)
+    device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+    attacker = create_attacker(attacker_name)
 
-    if algorithm_name == 'FGSM':
-        fgsmAdversarialExample(model, data_loader, checkpoint_name, path_to_dataset, epsilons)
+    accuracies = []
+    examples = []
+    for epsilon in epsilons:
+        acc, ex = _findAdversarialExampleInternal(model, data_loader, device, attacker, epsilon)
+        accuracies.append(acc)
+        examples.append(ex)
+
+    plt.figure(figsize=(5, 5))
+    plt.plot(epsilons, accuracies, "*-")
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.xticks(np.arange(0, max(epsilons) + 0.05, step=0.05))
+    plt.title("Accuracy vs Epsilon")
+    plt.xlabel("Epsilon")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+
+def create_attacker(attacker_name):
+    if attacker_name == 'FGSM':
+        return fgsm_attack
     else:
-        raise Exception('Error: unsupported algorithm_name:', algorithm_name)
+        raise Exception('Error: unsupported algorithm_name:', attacker_name)
 
 
-def _findAdversarialExampleInternal():
-    pass
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
 
 
-def test1(model, device, test_loader, epsilon):
-
-    # Accuracy counter
+def _findAdversarialExampleInternal(model, data_loader, device, attacker, epsilon):
     correct = 0
     adv_examples = []
 
-    # Loop over all examples in test set
-    for data, target in test_loader:
+    for data, target in data_loader:
 
         # Send the data and label to the device
         data, target = data.to(device), target.to(device)
@@ -206,66 +230,38 @@ def test1(model, device, test_loader, epsilon):
         if init_pred.item() != target.item():
             continue
 
-        # Calculate the loss
         loss = F.nll_loss(output, target)
 
-        # Zero all existing gradients
         model.zero_grad()
 
         # Calculate gradients of model in backward pass
         loss.backward()
 
-        # Collect datagrad
-        data_grad = data.grad.data
-
-        # Call FGSM Attack
-        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+        perturbed_data = attacker(data, epsilon, data.grad.data)
 
         # Re-classify the perturbed image
         output = model(perturbed_data)
 
         # Check for success
-        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        final_pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
         if final_pred.item() == target.item():
             correct += 1
             # Special case for saving 0 epsilon examples
             if (epsilon == 0) and (len(adv_examples) < 5):
                 adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+                adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
         else:
             # Save some adv examples for visualization later
             if len(adv_examples) < 5:
                 adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+                adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
 
     # Calculate final accuracy for this epsilon
-    final_acc = correct/float(len(test_loader))
-    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+    final_acc = correct/float(len(data_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(data_loader), final_acc))
 
     # Return the accuracy and an adversarial example
     return final_acc, adv_examples
-
-
-def fgsmAdversarialExample(model, data_loader, checkpoint_name, path_to_dataset, epsilons):
-    accuracies = []
-    examples = []
-
-    # Run test for each epsilon
-    for eps in epsilons:
-        acc, ex = test1(model, device, test_loader, eps)
-        accuracies.append(acc)
-        examples.append(ex)
-
-
-def fgsm_attack(image, epsilon, data_grad):
-    # Collect the element-wise sign of the data gradient
-    sign_data_grad = data_grad.sign()
-    # Create the perturbed image by adjusting each pixel of the input image
-    perturbed_image = image + epsilon*sign_data_grad
-    # Adding clipping to maintain [0,1] range
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    # Return the perturbed image
-    return perturbed_image
 
 
 def main():
